ad91f3a0a73abc59f52b967120b2a0019439476e
Robert Tanase <robert.tanase97@gmail.com>
2021-06-29 21:24:59 +0100

    Initial Commit

---
+++ MapsPeople Sample Integration iOS/MapsPeople_Sample_Integration_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapsPeople_Sample_Integration_iOSApp.swift
+//  MapsPeople Sample Integration iOS
+//
+//  Created by Robert Tanase on 29/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct MapsPeople_Sample_Integration_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
